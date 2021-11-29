49bf817a25003fa37fd8889ffbd1c12454b0e40d
Vernon Bengtson <87542373+VABIII@users.noreply.github.com>
2021-11-29 12:14:21 -0600

    Initial Save and Submit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React from 'react';
 
+const todosList = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
+class TodoList extends React.Component {
+  render() {
+    return(
+        <div>
+          <h2>Welcome to your Todo App!</h2>
+        </div>
+    )
+  }
+}
+
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      todos: todosList
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
+      <TodoList todos={this.state.todos}/>
     );
   }
 }
